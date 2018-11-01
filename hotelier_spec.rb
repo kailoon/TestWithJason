fefba0d862e1209edd13a81df24238d624ef61d3
kailoon <chankailoon@gmail.com>
2018-11-01 15:47:32 +0800

    Lesson 2 & 3
    Adds room number and availbility checking

--- hotelier_spec.rb
+++ hotelier_spec.rb
@@ -4,11 +4,15 @@ class Hotel
   attr_accessor :guests
 
   def initialize
-    @guests =[]
+    @guests = []
+    @occupied_rooms = []
   end
 
-  def check_in_guest(guest_name)
+  def check_in_guest(guest_name,room_number)
+    return false if @occupied_rooms.include?(room_number)
     @guests << guest_name
+    @occupied_rooms << room_number
+    true
   end
 
   def check_out_guest(guest_name)
@@ -17,22 +21,40 @@ class Hotel
 end
 
 describe Hotel do
-  it 'can check in a guest' do
-    hotel = Hotel.new
-    hotel.check_in_guest('George Harrison')
-    expect(hotel.guests).to include 'George Harrison'
+  let (:hotel) { Hotel.new }
+  describe 'checking in guest' do
+
+    it 'adds the guest into the guest list' do
+      hotel.check_in_guest('George Harrison',88)
+      expect(hotel.guests).to include 'George Harrison'
+    end
+
+    context 'room is available' do
+      it 'allows check in' do
+        expect(hotel.check_in_guest('George Harrison',88)).to be true
+      end
+    end
+
+    context 'room is not available' do
+      it 'disallows check-in' do
+        hotel.check_in_guest('George Harrison',88)
+        expect(hotel.check_in_guest('John Doe',88)).to be false
+      end
+    end
+
+    it 'does not add the guest to the guest list' do
+      hotel.check_in_guest('Jordan',44)
+      hotel.check_in_guest('Kobe',44)
+      expect(hotel.guests).not_to include 'Kobe'
+    end
   end
 
   it 'can check out a guest' do
     # instantiate a hotel
-    hotel = Hotel.new
-
     # check a guest into the hotel
-    hotel.check_in_guest('Michael')
-
+    hotel.check_in_guest('Michael',99)
     # check the same guest out of the hotel
     hotel.check_out_guest('Michael')
-
     # expect that the hotel's guests do not include that guest
     expect(hotel.guests).not_to include 'Michael'
   end
